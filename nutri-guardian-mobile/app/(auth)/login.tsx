fbc57ead432c2b7afc892a97d17cefcba2a28f46
Dematto04 <mlong200004@gmail.com>
2025-06-14 19:39:53 +0700

    api

--- nutri-guardian-mobile/app/(auth)/login.tsx
+++ nutri-guardian-mobile/app/(auth)/login.tsx
@@ -3,6 +3,7 @@ import { ThemedInput } from "@/components/ThemedInput";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Colors } from "@/constants/Colors";
+import axios from "axios";
 import { router } from "expo-router";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -10,17 +11,54 @@ import { StyleSheet, Text, View } from "react-native";
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  const handleLogin = () => {
-    console.log("Login attempt with:", { email, password });
-    router.replace("/(tabs)/education");
+  // console.log(process.env.EXPO_PUBLIC_API_URL);
+
+  // const handleLogin = () => {
+  //   console.log("123");
+
+  //   fetch("https://localhost:7087/api/authentication/login", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ email, password }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((res) => {
+  //       console.log({ res });
+  //     })
+  //     .catch((error) => {
+  //       console.log("Login error:", error);
+  //     });
+  // };
+  const handleLogin = async () => {
+    try {
+      // const res = await AuthService.login({ email, password });
+      const res = await axios.post("http://10.0.2.2:7087/api/authentication/login", {email, password})
+      console.log("123");
+      
+      // const res = await fetch("http://192.168.1.6:7087/api/authentication/login", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({ email, password }),
+      // })
+      //   .then((res) => res.json())
+      //   .then((res) => {
+      //     console.log({ res });
+      //   });
+    } catch (error) {
+      console.log({ error });
+    }
+    // router.replace("/(tabs)/education");
   };
 
   return (
     <ThemedView style={styles.container}>
       <View style={styles.formContainer}>
-        <ThemedText type="subtitle" style={styles.title}>Welcome Back!</ThemedText>
-        
+        <ThemedText type="subtitle" style={styles.title}>
+          Welcome Back!
+        </ThemedText>
+
         <View style={styles.inputContainer}>
           <ThemedInput
             placeholder="Email"
@@ -37,20 +75,17 @@ export default function LoginScreen() {
           />
         </View>
 
-        <ThemedButton
-          title="Login"
-          onPress={handleLogin}
-        />
+        <ThemedButton title="Login" onPress={handleLogin} />
 
         <View style={styles.footer}>
           <Text style={[styles.footerText, { color: Colors.text.secondary }]}>
-            Don't have an account? 
+            Don't have an account?
           </Text>
           <View>
-            <ThemedButton 
+            <ThemedButton
               title="Register"
               variant="secondary"
-              onPress={() => router.push('/(auth)/register')}
+              onPress={() => router.push("/(auth)/register")}
             />
           </View>
         </View>
@@ -62,31 +97,31 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
   },
   formContainer: {
-    width: '100%',
+    width: "100%",
     maxWidth: 400,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   title: {
     fontSize: 24,
     marginBottom: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
   inputContainer: {
     gap: 15,
     marginBottom: 20,
   },
   footer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: "center",
     gap: 10,
   },
   footerText: {
     color: Colors.text.secondary,
   },
-}); 
+});
